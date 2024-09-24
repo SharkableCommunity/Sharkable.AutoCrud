45b676e27e324c6a2a1487f73f90cb6bbb2dda92
charleypeng <charleypeng@gmail.com>
2024-09-24 12:30:11 +0800

    add sql service extensions

---
+++ AddAutoCrudExtension.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SqlSugar;
+
+namespace Sharkable.AutoCrud.SqlSugar;
+
+public static class AutoCrudExtension
+{
+    public static IServiceCollection AddSqlSugar(this IServiceCollection services, Action<SqlSugarOptions>? setupOption = null)
+    {
+        var opt = new SqlSugarOptions();
+        setupOption?.Invoke(opt);
+        services.Configure<SqlSugarOptions>(o => o = opt);
+        StaticConfig.EnableAot = true;
+        var conf = new ConnectionConfig
+        {
+            IsAutoCloseConnection = opt.IsAutoCloseConnection,
+            DbType = (global::SqlSugar.DbType)opt.DbType,
+            ConnectionString = opt.ConnectionString,
+            ConfigId = opt.ConfigId
+        };
+        SqlSugarScope sqlSugar = new(conf);
+        services.AddSingleton<ISqlSugarClient>(sqlSugar);
+        return services;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Sharkable.AutoCrud
 automatic generate crud operations api for Sharkable based minimal api framework
+
+### Note this is a Sharkable based extension and WIP, just for testing users
+
+## Usage
+```csharp
+//in your sharkable based program for aot mode
+builder.Services.AddShark([typeof(Program).Assembly, typeof(Sharkable.AutoCrudSqlSugar).Assembly], opt=>{
+    opt.Format = Sharkable.EndpointFormat.Tolower;
+    opt.ConfigureAutoCrud(s =>
+    {
+        s.IsAutoCloseConnection = true;
+        s.DbType = DbType.Sqlite;
+        s.ConnectionString = "DataSource=testsample.db";
+    });
+});
+```
